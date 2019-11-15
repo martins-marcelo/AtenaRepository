4f9c3fd667e16c54fa71c03e7c6a2098a546e6bd
martins-marcelo <bfmarcelo1@gmail.com>
2019-11-15 13:50:02 -0500

    Obtencao de JSON e escrita em CSV
    Adicionada a possibilidade de obtenção do arquivo JSON e transformação
    para CSV

--- Atena/src/com/marcelomartins/atena/controllers/BuscaController.java
+++ Atena/src/com/marcelomartins/atena/controllers/BuscaController.java
@@ -1,11 +1,27 @@
 package com.marcelomartins.atena.controllers;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import com.marcelomartins.atena.controllers.converters.JSONFlattener;
 
 public class BuscaController {
-	public void buscar(String rep) {
-		//Fazer a busca do arquivo JSON atraves da url fornecida
-		//URL url = new  URL("https://api.github.com/repos/"+rep+"/pulls");
+	private Integer pullNumber; //Number of a specific pull request
+	private Integer numberOfPulls; //Number of commits associated to one specific pull request
+	
+	public void buscarRepositorio(String rep) throws Exception{
+		/*
+		 * Criando JSON a partir de URL
+		 */
+		List<Map<String, String>> flatJson = JSONFlattener.parseJson(
+			new URI(
+				"https://api.github.com/repos/"+rep+"/pulls?state=closed"
+			)
+		);
 		  
 	}
+	
 }
