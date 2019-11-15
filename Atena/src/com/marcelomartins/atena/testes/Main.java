6502d436750d985b1b76cd031d8f810b4a5c0289
Marcelo <Marcelo@Nascimento-PC>
2019-11-14 22:23:06 -0500

    Correção de package
    Foi corrigido o caminho do pacote do arquivo Main.java da classe de
    testes

--- Atena/src/com/marcelomartins/atena/testes/Main.java
+++ Atena/src/com/marcelomartins/atena/testes/Main.java
@@ -14,7 +14,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package org.jsontocsv;
+package com.marcelomartins.atena.testes;
 
 import java.io.File;
 import java.net.URI;
