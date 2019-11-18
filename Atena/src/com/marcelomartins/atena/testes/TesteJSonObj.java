1cfbc75a3e98f9e2a0a2b23b84f57e8c127a70c5
erickfn99 <erickfernandess2@gmail.com>
2019-11-18 12:00:36 -0500

    Alteração no POM do projeto

--- Atena/src/com/marcelomartins/atena/testes/TesteJSonObj.java
+++ Atena/src/com/marcelomartins/atena/testes/TesteJSonObj.java
@@ -16,18 +16,30 @@ public class TesteJSonObj{
 	public static void main(String[] args) throws IOException {
 		URL url = new URL("https://api.github.com/repos/spring-by-example/spring-by-example");
 
-		InputStream is = url.openStream();
-		JsonReader reader = Json.createReader(is);	
-		
-		JsonObject obj = reader.readObject();
-		JsonArray results = obj.getJsonArray("data");
-		
-		for (JsonObject result : results.getValuesAs(JsonObject.class)) {
-			System.out.println(result.getJsonObject("from").getString("name"));
-			System.out.println(": ");
-			
+//		InputStream is = url.openStream();
+//		JsonReader reader = Json.createReader(is);	
+//		
+//		JsonObject obj = reader.readObject();
+//		JsonArray results = obj.getJsonArray("data");
+//		
+//		for (JsonObject result : results.getValuesAs(JsonObject.class)) {
+//			System.out.println(result.getJsonObject("from").getString("name"));
+//			System.out.println(": ");
+//			
+//		}
+		try{
+			InputStream ins = url.openStream();
+			JsonReader rdr = Json.createReader(ins);
+			JsonObject obj = rdr.readObject();
+			JsonArray results = obj.getJsonArray("data");
+			for (JsonObject result : results.getValuesAs(JsonObject.class)) {
+				System.out.println(result.getJsonObject("from").getString("name"));
+				System.out.println(": ");
+				System.out.println();
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
-		
 
 
 	}
