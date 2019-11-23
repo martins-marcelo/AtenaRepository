1a420fb125409c3b3a49e2e0668ed5a8a2778adf
Marcelo <bfmarcelo1@gmail.com>
2019-11-23 06:16:43 -0500

    Apriori e discretizacao
    Ta com erro mas é isso ai, sem paciencia pra mensagem

--- Atena/src/com/marcelomartins/atena/controllers/mineradores/Associador.java
+++ Atena/src/com/marcelomartins/atena/controllers/mineradores/Associador.java
@@ -4,19 +4,37 @@ import java.nio.file.Path;
 
 import weka.associations.Apriori;
 import weka.core.Instances;
+import weka.core.converters.CSVSaver;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Discretize;
 
 public class Associador {
 	DataSource ds;
-	Instances ins;
+	Instances ins, discret;
 	Apriori ap;
+	Discretize disc;
+	String[] options;
 	public void minerarApriori(Path caminho) throws Exception {
+		options = new String[5];
+		options[0] = "-B";
+		options[1] = "5";
+		options[2] = "-R";
+		options[3] = "1-9";
+		options[4] = "-V";
 		ds = new DataSource("pullrequests.csv");
 		ins = ds.getDataSet();
 		ins.setClassIndex(7);
 		System.out.println("numero de atributos"+ins.numAttributes());
 		System.out.println("numero de instancias"+ins.numInstances());
+		disc = new Discretize();
+		disc.setOptions(options);
+		disc.setInputFormat(ins);
+		discret = Filter.useFilter(ins, disc);
 		ap = new Apriori();
+		ap.buildAssociations(discret);
+		System.out.println(ap);
+		
 		
 	}
 }
