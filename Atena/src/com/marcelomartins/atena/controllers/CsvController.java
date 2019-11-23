7e5a248fb3dc245a7e490f516241ce6d00e04364
Marcelo <bfmarcelo1@gmail.com>
2019-11-22 23:32:41 -0500

    Escrita e recuperação de CSV
    Foi trocada a versão da dependencia do pom, agora funciona normal.
    O sistema ja escreve e recupera os arquivos

--- Atena/src/com/marcelomartins/atena/controllers/CsvController.java
+++ Atena/src/com/marcelomartins/atena/controllers/CsvController.java
@@ -4,20 +4,23 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
+import com.marcelomartins.atena.controllers.mineradores.Associador;
 import com.marcelomartins.atena.domain.PullRequest;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
 
 public class CsvController {
-	public void parseAndWrite(List<PullRequest> lstPulls) throws Exception{ 
-//	    String path = new File("").getAbsolutePath();;
-        Writer writer = Files.newBufferedWriter(Paths.get("pessoas.csv"));
+	public void parseAndWrite(List<PullRequest> lstPulls) throws Exception{
+		Associador as = new Associador();
+        Writer writer = Files.newBufferedWriter(Paths.get("pullrequests.csv"));
         StatefulBeanToCsv<PullRequest> beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
 
         beanToCsv.write(lstPulls);
 
         writer.flush();
         writer.close();
-	     System.out.println("Finished! Go, look");
+        System.out.println("Csv file created!");
+        as.minerarApriori(Paths.get("pullrequests.csv"));
 	}
 }
--- Atena/src/com/marcelomartins/atena/controllers/mineradores/Associador.java
+++ Atena/src/com/marcelomartins/atena/controllers/mineradores/Associador.java
@@ -1,9 +1,22 @@
 package com.marcelomartins.atena.controllers.mineradores;
 
+import java.nio.file.Path;
+
 import weka.associations.Apriori;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class Associador {
-	public void minerarApriori() {
-		Apriori ap = new Apriori();
+	DataSource ds;
+	Instances ins;
+	Apriori ap;
+	public void minerarApriori(Path caminho) throws Exception {
+		ds = new DataSource("pullrequests.csv");
+		ins = ds.getDataSet();
+		ins.setClassIndex(7);
+		System.out.println("numero de atributos"+ins.numAttributes());
+		System.out.println("numero de instancias"+ins.numInstances());
+		ap = new Apriori();
+		
 	}
 }
